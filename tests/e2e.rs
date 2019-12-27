ec8889598d89d4cb5f0946ce39f09edabbdcb53b
Mark Benvenuto <mark.benvenuto@mongodb.com>
2019-12-27 18:58:56 -0500

    snap

--- tests/e2e.rs
+++ tests/e2e.rs
@@ -7,21 +7,22 @@ See https://rust-cli.github.io/book/tutorial/testing.html
 
 https://github.com/droundy/roundqueue/blob/master/tests/lib.rs
 */
-extern crate kmip_client;
+//extern crate kmip_client;
 //extern crate main;
 
 #[test]
 fn test_create_request3() {
-    let bytes = vec![
-        0x42, 0x00, 0x78];
+    // let bytes = vec![
+    //     0x42, 0x00, 0x78];
 
-    let store = Arc::new(KmipMemoryStore::new());
+    // let store = Arc::new(KmipMemoryStore::new());
 
-    let server_context = ServerContext::new(store);
+    // let server_context = ServerContext::new(store);
 
-    let mut rc = RequestContext::new(&server_context);
-    process_kmip_request(&mut rc, bytes.as_slice());
+    // let mut rc = RequestContext::new(&server_context);
+    // process_kmip_request(&mut rc, bytes.as_slice());
 
-    process_kmip_request(&mut rc, get_bytes.as_slice());
+    // process_kmip_request(&mut rc, get_bytes.as_slice());
 
+    println!("Hello");
 }
--- tests_e2e/src/lib.rs
+++ tests_e2e/src/lib.rs
@@ -1,2 +1,20 @@
-extern crate  kmip_client;
+extern crate kmip_client;
+extern crate server;
 
+
+#[test]
+fn test_create_request3() {
+    // let bytes = vec![
+    //     0x42, 0x00, 0x78];
+
+    // let store = Arc::new(KmipMemoryStore::new());
+
+    // let server_context = ServerContext::new(store);
+
+    // let mut rc = RequestContext::new(&server_context);
+    // process_kmip_request(&mut rc, bytes.as_slice());
+
+    // process_kmip_request(&mut rc, get_bytes.as_slice());
+
+    println!("Hello");
+}
